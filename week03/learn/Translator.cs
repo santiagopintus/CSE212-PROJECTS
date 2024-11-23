c662d326e1b023b0f32bc6457b8ea8a2e6d713d9
Santiago Pintus <santiagopintus@gmail.com>
2024-11-23 15:47:13 -0300

    Implemented Translator

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 public class Translator
 {
     public static void Run()
@@ -16,7 +18,7 @@ public class Translator
     /// <summary>
     /// Add the translation from 'from_word' to 'to_word'
     /// For example, in a english to german dictionary:
-    /// 
+    ///
     /// my_translator.AddWord("book","buch")
     /// </summary>
     /// <param name="fromWord">The word to translate from</param>
@@ -24,7 +26,7 @@ public class Translator
     /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words[fromWord] = toWord;
     }
 
     /// <summary>
@@ -34,7 +36,10 @@ public class Translator
     /// <returns>The translated word or "???" if no translation is available</returns>
     public string Translate(string fromWord)
     {
-        // ADD YOUR CODE HERE
-        return "";
+        if (_words.ContainsKey(fromWord))
+        {
+            return _words[fromWord];
+        }
+        return "???";
     }
 }

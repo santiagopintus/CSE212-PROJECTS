2e7c0925a8e3581f53a5d96bafc94854147102b3
Santiago Pintus <santiagopintus@gmail.com>
2024-12-10 00:26:11 -0300

    implemented first 4 functions

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Security.Cryptography;
 
 public static class Recursion
 {
@@ -7,15 +8,16 @@ public static class Recursion
     /// # Problem 1 #
     /// #############
     /// Using recursion, find the sum of 1^2 + 2^2 + 3^2 + ... + n^2
-    /// and return it.  Remember to both express the solution 
-    /// in terms of recursive call on a smaller problem and 
+    /// and return it.  Remember to both express the solution
+    /// in terms of recursive call on a smaller problem and
     /// to identify a base case (terminating case).  If the value of
     /// n <= 0, just return 0.   A loop should not be used.
     /// </summary>
     public static int SumSquaresRecursive(int n)
     {
-        // TODO Start Problem 1
-        return 0;
+        if (n <= 0)
+            return 0;
+        return n * n + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -24,35 +26,51 @@ public static class Recursion
     /// #############
     /// Using recursion, insert permutations of length
     /// 'size' from a list of 'letters' into the results list.  This function
-    /// should assume that each letter is unique (i.e. the 
+    /// should assume that each letter is unique (i.e. the
     /// function does not need to find unique permutations).
     ///
     /// In mathematics, we can calculate the number of permutations
     /// using the formula: len(letters)! / (len(letters) - size)!
     ///
     /// For example, if letters was [A,B,C] and size was 2 then
-    /// the following would the contents of the results array after the function ran: AB, AC, BA, BC, CA, CB (might be in 
+    /// the following would the contents of the results array after the function ran: AB, AC, BA, BC, CA, CB (might be in
     /// a different order).
     ///
-    /// You can assume that the size specified is always valid (between 1 
+    /// You can assume that the size specified is always valid (between 1
     /// and the length of the letters list).
     /// </summary>
-    public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
+    public static void PermutationsChoose(
+        List<string> results,
+        string letters,
+        int size,
+        string word = ""
+    )
     {
-        // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+
+        for (int i = 0; i < letters.Length; i++)
+        {
+            string newWord = word + letters[i];
+            string remainingLetters = letters.Substring(0, i) + letters.Substring(i + 1);
+            PermutationsChoose(results, remainingLetters, size, newWord);
+        }
     }
 
     /// <summary>
     /// #############
     /// # Problem 3 #
     /// #############
-    /// Imagine that there was a staircase with 's' stairs.  
-    /// We want to count how many ways there are to climb 
-    /// the stairs.  If the person could only climb one 
-    /// stair at a time, then the total would be just one.  
-    /// However, if the person could choose to climb either 
-    /// one, two, or three stairs at a time (in any order), 
-    /// then the total possibilities become much more 
+    /// Imagine that there was a staircase with 's' stairs.
+    /// We want to count how many ways there are to climb
+    /// the stairs.  If the person could only climb one
+    /// stair at a time, then the total would be just one.
+    /// However, if the person could choose to climb either
+    /// one, two, or three stairs at a time (in any order),
+    /// then the total possibilities become much more
     /// complicated.  If there were just three stairs,
     /// the possible ways to climb would be four as follows:
     ///
@@ -64,24 +82,24 @@ public static class Recursion
     /// With just one step to go, the ways to get
     /// to the top of 's' stairs is to either:
     ///
-    /// - take a single step from the second to last step, 
-    /// - take a double step from the third to last step, 
+    /// - take a single step from the second to last step,
+    /// - take a double step from the third to last step,
     /// - take a triple step from the fourth to last step
     ///
-    /// We don't need to think about scenarios like taking two 
+    /// We don't need to think about scenarios like taking two
     /// single steps from the third to last step because this
     /// is already part of the first scenario (taking a single
     /// step from the second to last step).
     ///
     /// These final leaps give us a sum:
     ///
-    /// CountWaysToClimb(s) = CountWaysToClimb(s-1) + 
+    /// CountWaysToClimb(s) = CountWaysToClimb(s-1) +
     ///                       CountWaysToClimb(s-2) +
     ///                       CountWaysToClimb(s-3)
     ///
     /// To run this function for larger values of 's', you will need
     /// to update this function to use memoization.  The parameter
-    /// 'remember' has already been added as an input parameter to 
+    /// 'remember' has already been added as an input parameter to
     /// the function for you to complete this task.
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
@@ -97,9 +115,19 @@ public static class Recursion
             return 4;
 
         // TODO Start Problem 3
+        if (remember == null){
+          remember = [];
+        } else {
+        /* Read remembered value if it exists */
+          if (remember.TryGetValue(s, out decimal value)) {
+            return value;
+          }
+        }
 
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1,remember) + CountWaysToClimb(s - 2,remember) + CountWaysToClimb(s - 3,remember);
+        if (ways > 0)
+            remember.Add(s, ways);
         return ways;
     }
 
@@ -107,32 +135,55 @@ public static class Recursion
     /// #############
     /// # Problem 4 #
     /// #############
-    /// A binary string is a string consisting of just 1's and 0's.  For example, 1010111 is 
-    /// a binary string.  If we introduce a wildcard symbol * into the string, we can say that 
-    /// this is now a pattern for multiple binary strings.  For example, 101*1 could be used 
-    /// to represent 10101 and 10111.  A pattern can have more than one * wildcard.  For example, 
+    /// A binary string is a string consisting of just 1's and 0's.  For example, 1010111 is
+    /// a binary string.  If we introduce a wildcard symbol * into the string, we can say that
+    /// this is now a pattern for multiple binary strings.  For example, 101*1 could be used
+    /// to represent 10101 and 10111.  A pattern can have more than one * wildcard.  For example,
     /// 1**1 would result in 4 different binary strings: 1001, 1011, 1101, and 1111.
-    ///	
-    /// Using recursion, insert all possible binary strings for a given pattern into the results list.  You might find 
+    ///
+    /// Using recursion, insert all possible binary strings for a given pattern into the results list.  You might find
     /// some of the string functions like IndexOf and [..X] / [X..] to be useful in solving this problem.
     /// </summary>
     public static void WildcardBinary(string pattern, List<string> results)
     {
-        // TODO Start Problem 4
+        // Base case: if no wildcards, add the pattern to results
+        if (!pattern.Contains('*'))
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        // Find the first wildcard
+        int wildcardIndex = pattern.IndexOf('*');
+
+        // Try replacing the wildcard with '0'
+        string pattern0 = pattern.Substring(0, wildcardIndex) + '0' + pattern.Substring(wildcardIndex + 1);
+        WildcardBinary(pattern0, results);
+
+        // Try replacing the wildcard with '1'
+        string pattern1 = pattern.Substring(0, wildcardIndex) + '1' + pattern.Substring(wildcardIndex + 1);
+        WildcardBinary(pattern1, results);
     }
 
     /// <summary>
     /// Use recursion to insert all paths that start at (0,0) and end at the
     /// 'end' square into the results list.
     /// </summary>
-    public static void SolveMaze(List<string> results, Maze maze, int x = 0, int y = 0, List<ValueTuple<int, int>>? currPath = null)
+    public static void SolveMaze(
+        List<string> results,
+        Maze maze,
+        int x = 0,
+        int y = 0,
+        List<ValueTuple<int, int>>? currPath = null
+    )
     {
         // If this is the first time running the function, then we need
         // to initialize the currPath list.
-        if (currPath == null) {
+        if (currPath == null)
+        {
             currPath = new List<ValueTuple<int, int>>();
         }
-        
+
         // currPath.Add((1,2)); // Use this syntax to add to the current path
 
         // TODO Start Problem 5
